245bfc9b9f746f058edaa838dc7391ab47f7f0df
sumaiyakhn1 <sumaiyakn28@gmail.com>
2025-11-20 00:16:52 +0530

    done

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function App() {
 
         <div className="search-input-wrapper">
           <input
-            placeholder="Search by Scholar ID, name or mobile — e.g. 2172 or Aarav"
+            placeholder="Search by Scholar ID, name or mobile — e.g. 2074 or Priyal"
             value={scholarId}
             onChange={(e) => setScholarId(e.target.value)}
             onKeyDown={(e) => e.key === "Enter" && fetchStudent()}
